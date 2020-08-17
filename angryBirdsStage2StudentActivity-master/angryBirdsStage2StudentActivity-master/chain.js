085ad548559fe927e3eb17cd5ce330dd6ca14a47
hpandya837 <66964248+hpandya837@users.noreply.github.com>
2020-08-17 10:22:57 -0400

    Add files via upload

---
+++ angryBirdsStage2StudentActivity-master/angryBirdsStage2StudentActivity-master/chain.js
@@ -0,0 +1,24 @@
+class Chain {
+    constructor(
+        bodyA,bodyB
+    )
+    {
+        var options = {
+        bodyA:bodyA,
+        bodyB:bodyB,
+        length: 10,
+        stiffness: 0.04
+
+        }
+        this.body=Constraint.create(options)
+        World.add(world,this.body)
+    }
+
+
+display(){
+var pointA=this.body.bodyA.position
+var pointB=this.body.bodyB.position
+strokeWeight(5) 
+line(pointA.x,pointA.y,pointB.x,pointB.y)   
+}
+}
